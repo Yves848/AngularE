0b5d7635ab11f525322af842c5f4c5b74d5cbdd3
Yves Godart <yves.godart@gmail.com>
2021-05-02 19:52:37 +0200

    primeComponents

--- src/app/components/primeComponents.ts
+++ src/app/components/primeComponents.ts
@@ -46,20 +46,49 @@ import { PanelModule } from 'primeng/panel';
 import { SplitterModule } from 'primeng/splitter';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { TabViewModule } from 'primeng/tabview';
-import {ToolbarModule} from 'primeng/toolbar';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
-import {ConfirmPopupModule} from 'primeng/confirmpopup';
-import {DialogModule} from 'primeng/dialog';
-import {DynamicDialogModule} from 'primeng/dynamicdialog';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
-import {SidebarModule} from 'primeng/sidebar';
-import {TooltipModule} from 'primeng/tooltip';
-import {FileUploadModule} from 'primeng/fileupload';
-import {HttpClientModule} from '@angular/common/http';
-import {MenuModule} from 'primeng/menu';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
-import {ContextMenuModule} from 'primeng/contextmenu';
-import {MegaMenuModule} from 'primeng/megamenu';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { SidebarModule } from 'primeng/sidebar';
+import { TooltipModule } from 'primeng/tooltip';
+import { FileUploadModule } from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuModule } from 'primeng/menu';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { MenubarModule } from 'primeng/menubar';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { SlideMenuModule } from 'primeng/slidemenu';
+import { StepsModule } from 'primeng/steps';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { ChartModule } from 'primeng/chart';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { CarouselModule } from 'primeng/carousel';
+import { GalleriaModule } from 'primeng/galleria';
+import { DragDropModule } from 'primeng/dragdrop';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import {BadgeModule} from 'primeng/badge';
+import {BlockUIModule} from 'primeng/blockui';
+import {CaptchaModule} from 'primeng/captcha';
+import { ChipModule } from 'primeng/chip';
+import {InplaceModule} from 'primeng/inplace';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {ScrollTopModule} from 'primeng/scrolltop';
+import {SkeletonModule} from 'primeng/skeleton';
+import { TagModule } from 'primeng/tag';
+import {TerminalModule} from 'primeng/terminal';
+
+
+
+
 
 
 
@@ -133,7 +162,32 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     MenuModule,
     BreadcrumbModule,
     ContextMenuModule,
-    MegaMenuModule
+    MegaMenuModule,
+    MenubarModule,
+    PanelMenuModule,
+    SlideMenuModule,
+    StepsModule,
+    TabMenuModule,
+    TieredMenuModule,
+    ChartModule,
+    MessagesModule,
+    MessageModule,
+    CarouselModule,
+    GalleriaModule,
+    DragDropModule,
+    AvatarModule,
+    AvatarGroupModule,
+    BadgeModule,
+    BlockUIModule,
+    CaptchaModule,
+    ChipModule,
+    InplaceModule,
+    ProgressBarModule,
+    ProgressSpinnerModule,
+    ScrollTopModule,
+    SkeletonModule,
+    TagModule,
+    TerminalModule
   ]
 })
 export class PrimeComponentsModule { }
